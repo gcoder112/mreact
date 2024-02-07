2ccdce3ccd2dd0c7f5a40c68692568370d649ffe
ubtrel <ubtrel>
2024-02-07 10:54:59 -0800

    Add examples of type script in react for number and boolean types

--- _/ts/react-typescript-demo/src/App.tsx
+++ _/ts/react-typescript-demo/src/App.tsx
@@ -3,7 +3,7 @@ import { Greet } from './components/Greet';
 function App() {
   return (
     <div className="App">
-      <Greet name='Alex'/>
+      <Greet name='Alex' messageCount={20} isLoggedIn={false}/>
     </div>
   );
 }
