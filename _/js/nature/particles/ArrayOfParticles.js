282e4e0a62290a1c04e6f0a9bccc0a176b8b4d08
peter <peter>
2025-05-29 12:26:33 -0700

    Update particle system examples

--- _/js/nature/particles/ArrayOfParticles.js
+++ _/js/nature/particles/ArrayOfParticles.js
@@ -1,19 +1,26 @@
-let total = 50;
+// let total = 50;
 let particles = [];
 
 function setup() {
   createCanvas(640, 640);
-  for (let i=0; i < total; i++) {
-    particles[i] = new Particle(width / 2, height / 4);
-  }
+  // for (let i=0; i < total; i++) {
+  //   particles[i] = new Particle(width / 2, height / 4);
+  // }
 }
 
 function draw() {
   background(255);
-  for (let i = 0; i < particles.length; i++) {
+  particles.push(new Particle(width / 2, 20));
+  for (let i = particles.length - 1; i >= 0; i--) {
     let particle = particles[i];
     particle.run();
+    if (particle.isDead()) {
+      particles.splice(i, 1);
+    }
   }
+  // for (let particle of particles) {
+  //   particle.run();
+  // }
 }
 
 class Particle {
